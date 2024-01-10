04f62e45819777bc2670cd28975bbe41ef4478ad
Rasel Hossen <raselhossen052@gmail.com>
2024-01-10 23:23:36 +0600

    class-1.9

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,26 +1,35 @@
 function Item({ name, isPacked }) {
-    let itemContent = "";
+  let itemContent = '';
 
-    if (isPacked) {
-        itemContent = <span>{name + "✔"}</span>;
-    } else {
-        itemContent = <span>{name}</span>;
-    }
+  if (isPacked) {
+    itemContent = <span>{name + '✔'}</span>;
+  } else {
+    itemContent = <span>{name}</span>;
+  }
 
-    return <li className="item">{itemContent}</li>;
+  return <li className='item'>{itemContent}</li>;
 }
 
 export default function PackingList() {
-    return (
-        <section>
-            <h1>Sally Rides Packing List</h1>
-            <ul>
-                <Item isPacked={true} name="Space suit" />
-                <Item isPacked={true} name="Helmet with a golden leaf" />
-                <Item isPacked={false} name="Photo of Tam" />
-            </ul>
-        </section>
-    );
+  return (
+    <section>
+      <h1>Sally Rides Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name='Space suit'
+        />
+        <Item
+          isPacked={true}
+          name='Helmet with a golden leaf'
+        />
+        <Item
+          isPacked={false}
+          name='Photo of Tam'
+        />
+      </ul>
+    </section>
+  );
 }
 
 // just for memory
