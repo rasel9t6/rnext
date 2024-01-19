a8d8e16b74c153af8ed18453f4879de0c06df64d
Rasel Hossen <raselhossen052@gmail.com>
2024-01-19 18:10:22 +0600

    visual_states

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Form from "./components/Form";
-import "./styles.css";
+import Form from './components/Form';
+import './styles.css';
 
-const statuses = ["empty", "typing", "submitting", "success", "error"];
+const statuses = ['empty', 'typing', 'submitting', 'success', 'error'];
 
 function App() {
-    return (
-        <div>
-            {statuses.map((status) => (
-                <Form key={status} status={status} />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      {statuses.map((status) => (
+        <Form
+          key={status}
+          status={status}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,27 @@
 // eslint-disable-next-line react/prop-types
-export default function Form({ status = "empty" }) {
-    // visual states: empty, typing, submitting, success, error
-
-    if (status === "success")
-        return (
-            <>
-                <h1>Thats right!</h1>
-                <hr />
-            </>
-        );
-
+export default function Form({ status = 'empty' }) {
+  // visual states: empty, typing, submitting, success, error
+  if (status === 'success')
     return (
-        <>
-            <h2>City quiz</h2>
-            <p>What city is located on two continents?</p>
-            <form id="form">
-                <textarea disabled={status === "submitting"}></textarea>
-                <br />
-                <button
-                    disabled={status === "empty" || status === "submitting"}
-                >
-                    Submit
-                </button>
-                {status === "submitting" && <p>Loading...</p>}
-                {status === "error" && (
-                    <p className="Error">There was an error</p>
-                )}
-            </form>
-            <hr />
-        </>
+      <>
+        <h1>Thats right!</h1>
+        <hr />
+      </>
     );
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>What city is located on two continents?</p>
+      <form id='form'>
+        <textarea disabled={status === 'submitting'}></textarea>
+        <br />
+        <button disabled={status === 'submitting' || status === 'empty'}>
+          Submit
+        </button>
+        {status === 'submitting' && <p>Loading...</p>}
+        {status === 'error' && <p className='Error'>There was an error</p>}
+      </form>
+      <hr />
+    </>
+  );
 }
